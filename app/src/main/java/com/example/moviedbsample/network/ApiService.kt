db83a0e92db41fe5dd19120e98f1450dd19cc079
nabzi <zeinab.modir@gmail.com>
2022-05-20 13:53:31 +0430

    Add okhttp logger

--- app/src/main/java/com/example/moviedbsample/network/ApiService.kt
+++ app/src/main/java/com/example/moviedbsample/network/ApiService.kt
@@ -4,6 +4,8 @@ import com.example.moviedbsample.model.Movie
 import com.example.moviedbsample.model.MovieListApiResult
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -22,13 +24,19 @@ private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+val client = OkHttpClient.Builder()
+    .addInterceptor(logger)
+    .build()
 /**
  * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
- * object.
- */
+ * object.*/
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
 
 
